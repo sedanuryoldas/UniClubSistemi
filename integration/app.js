e6e5f6404497369c31548ce23b918e895ed2d1db
MARIAM ABDELMAGID <m.abdelmagid2002@gmail.com>
2026-04-26 17:10:49 +0300

    Add integration app.js

---
+++ integration/app.js
@@ -0,0 +1,8 @@
+console.log("Integration started 🚀");
+
+const events = [
+  { id: 1, title: "AI Workshop", date: "2026-05-01" },
+  { id: 2, title: "Music Festival", date: "2026-05-03" }
+];
+
+console.log(events);
